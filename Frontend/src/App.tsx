cb8cdd64fc42b49fcac0e60c64b40fc85d6df0ff
rahulagowda277 <rahulgowda277@gmail.com>
2025-02-14 22:11:30 +0530

    finished integrating

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
 import Profile from "./pages/Profile";
-import UploadFile from "./pages/UploadFile"; // Import the UploadFile component
+import UploadFile from "./pages/UploadFile";
+import Community from "./pages/Community"; // Import the Community component
+import UserProfile from "./pages/UserProfile"; // Import the UserProfile component
 
 function App() {
   return (
@@ -12,8 +14,11 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/upload" element={<UploadFile />} />{" "}
-        {/* Add the route for UploadFile */}
+        <Route path="/upload" element={<UploadFile />} />
+        <Route path="/community" element={<Community />} />{" "}
+        {/* Add the route for Community */}
+        <Route path="/profile/:userId" element={<UserProfile />} />{" "}
+        {/* Add the route for UserProfile */}
       </Routes>
     </Router>
   );
