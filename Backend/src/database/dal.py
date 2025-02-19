0deac40281e51a40dd49e5fc9663a899940fa0ca
rahulagowda277 <rahulgowda277@gmail.com>
2025-02-19 19:12:59 +0530

    finished working on the database

--- Backend/src/database/dal.py
+++ Backend/src/database/dal.py
@@ -1,20 +1,20 @@
-from pymongo import MongoClient
 import sys
+from dotenv import load_dotenv
+from pymongo import MongoClient
 from utils.logger import logging
 from utils.exception import CustomException
 
 class Database:
     def __init__(self):
-        pass
-    
-    def Mongoconnector(self,):
-        try:
-            client = MongoClient("mongodb://localhost:27017/")
-            db = client["mydatabase"]
-            collection = db["mycollection"]
-            print("Connected to MongoDB successfully!")
-            return collection
-        except Exception as e:
-            raise CustomException(e,sys)
-    
-    logging.info("server started successfully")
+        load_dotenv()
+        self.client = None
+        self.db = None
+        self.userprofile = None
+        self.dataset = None
+        self.connect()
+
+    def connect(self):
+        self.client = MongoClient(os.getenv("MONGO_URI", "mongodb://localhost:27017/"))
+        self.db = self.client["mydatabase"]
+        self.userprofile = self.db["userprofiles"]
+        self.dataset = self.db["datasets"]
