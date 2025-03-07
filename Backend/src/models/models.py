64bd13eb5706688d42046075e559f2ef04e279ca
EquineNaveen <snaveengowda13063@gmail.com>
2025-03-07 23:09:59 +0530

    created schema for community page

--- Backend/src/models/models.py
+++ Backend/src/models/models.py
@@ -55,3 +55,26 @@ class UserProfile(BaseModel):
     number_of_raw_datasets: int = 0
     number_of_vectorized_datasets: int = 0
     # datasets: List[DatasetSummary] = Field(default_factory=list)
+
+class General(BaseModel):
+    id: str
+    title: str
+    description: str
+    uid: str
+    created_at: str
+
+
+class Issue(BaseModel):
+    id: str
+    title: str
+    description: str
+    uid: str
+    created_at: str
+
+class IssueReply(BaseModel):
+    id: str
+    issue_id: str
+    title: str
+    description: str
+    uid: str
+    created_at: str
