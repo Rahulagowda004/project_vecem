9720982077cc07fb40394c36f77eb68795a028e8
rahulagowda277 <rahulgowda277@gmail.com>
2025-03-12 22:12:41 +0530

    setup.py of library has been modified

--- library/setup.py
+++ library/setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="vecemlib",
+    name="vecem",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.0",
+        "zipfile"
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="vecem",
+    author_email="vectorembeddings@example.com",
     description="A library for downloading datasets from Vecem",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/vecemlib",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
 )
